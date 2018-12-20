74405253885be7e9250e4bfeafa03301f0801ab2
Jordan RENARD <jord.renard@gmail.com>
2018-12-20 19:26:12 +0100

    Improving random game

--- commands/rg.js
+++ commands/rg.js
@@ -1,10 +1,17 @@
+const fs = require("fs");
+
 module.exports = {
     name: "rg",
     description: "Vous ne savez pas à quoi jouer ? Demandez !",
     execute(message, args) {
         // const limit = parseInt(args.join(" "));
-        let games = ['League of Legends', 'Rocket League', 'Brawlhalla', 'H1Z1', 'PUBG', 'CS:Go', 'Dofus', 'Battlefield V', 'Fifa 18', 'Witch it', 'Business Tour', 'Minecraft', 'Gang Beast'];
+        /*let games = ['League of Legends', 'Rocket League', 'Brawlhalla', 'H1Z1', 'PUBG', 'CS:Go', 'Dofus', 'Battlefield V', 'Fifa 18', 'Witch it', 'Business Tour', 'Minecraft', 'Gang Beast'];
         const number = Math.floor((Math.random() * games.length) + 1);
-        message.channel.send('Vous devez jouer à : **' + games[number] + '**');
+        message.channel.send('Vous devez jouer à : **' + games[number] + '**');*/
+
+
+
+        const contentFolder = fs.readFileSync("./files/games.json");
+        message.channel.send(contentFolder);
     },
 };
