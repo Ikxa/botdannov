2b4c052d8f743debeef9ac2788d1393b3fd41463
Renard <jord.renard@gmail.com>
2018-12-20 15:01:21 +0100

    Faute

--- commands/rg.js
+++ commands/rg.js
@@ -5,6 +5,6 @@ module.exports = {
         // const limit = parseInt(args.join(" "));
         let games = ['League of Legends', 'Rocket League', 'Brawlhalla', 'H1Z1', 'PUBG', 'CS:Go', 'Dofus', 'Battlefield V', 'Fifa 18', 'Witch it', 'Business Tour'];
         const number = Math.floor((Math.random()    * games.length) + 1);
-        message.channel.send('Vous devez à jouer à : **' + games[number] + '**');
+        message.channel.send('Vous devez jouer à : **' + games[number] + '**');
     },
 };
