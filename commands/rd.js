0d8169fd8be6f78f3a0b32cb0d3a9439ea2abfe9
Renard <jord.renard@gmail.com>
2018-12-20 01:13:46 +0100

    Random number

--- commands/rd.js
+++ commands/rd.js
@@ -2,8 +2,10 @@ module.exports = {
     name: "rd",
     description: "Get a random number",
     execute(message, args) {
-        const limit = parseInt(args.join(" "));
-        let number = Math.floor((Math.random() * limit) + 1);
-        message.channel.send('Numéro aléatoire :' + number);
+        // const limit = parseInt(args.join(" "));
+        if (args.length > 0) {
+            const number = Math.floor((Math.random() * args[0]) + 1);
+            message.channel.send('Numéro aléatoire :' + number);
+        }
     },
 };
