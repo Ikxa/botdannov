26ea12ccc1e8b3fb76b689cc9dbd7f8a9516fea0
Renard <jord.renard@gmail.com>
2018-12-20 15:10:03 +0100

    Add new random game

--- commands/rg.js
+++ commands/rg.js
@@ -3,8 +3,8 @@ module.exports = {
     description: "Vous ne savez pas à quoi jouer ? Demandez !",
     execute(message, args) {
         // const limit = parseInt(args.join(" "));
-        let games = ['League of Legends', 'Rocket League', 'Brawlhalla', 'H1Z1', 'PUBG', 'CS:Go', 'Dofus', 'Battlefield V', 'Fifa 18', 'Witch it', 'Business Tour'];
-        const number = Math.floor((Math.random()    * games.length) + 1);
+        let games = ['League of Legends', 'Rocket League', 'Brawlhalla', 'H1Z1', 'PUBG', 'CS:Go', 'Dofus', 'Battlefield V', 'Fifa 18', 'Witch it', 'Business Tour', 'Minecraft', 'Gang Beast'];
+        const number = Math.floor((Math.random() * games.length) + 1);
         message.channel.send('Vous devez jouer à : **' + games[number] + '**');
     },
 };
