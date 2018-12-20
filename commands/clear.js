b69e3f5f5207420e930e19e5cad7c162d05c296b
Jordan RENARD <jord.renard@gmail.com>
2018-12-20 23:10:10 +0100

    Fix command purge

--- commands/clear.js
+++ commands/clear.js
@@ -1,18 +1,21 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
 module.exports = {
     name: "clear",
     description: "Supprimer un certain nombre de message",
     execute(message, args) {
-        if (args.length > 0) {
-            const deleteCount = parseInt(args[0], 10);
+        client.on("message", async => message => {
+            if (args.length > 0) {
+                const deleteCount = parseInt(args[0], 10);
 
-            if(!deleteCount || deleteCount < 2 || deleteCount > 100)
-                return message.reply("Merci d'indiquer un nombre en 1 et 10 message(s) à supprimer");
+                if(!deleteCount || deleteCount < 2 || deleteCount > 100)
+                    return message.reply("Merci d'indiquer un nombre en 1 et 10 message(s) à supprimer");
 
-            /*const fetched = message.channel.fetchMessages({limit: deleteCount});
-            message.channel.bulkDelete(fetched)
-                .catch(error => message.reply(`Je ne peux pas supprimer les messages car : ${error}`));*/
-
-            message.channel.send('' + deleteCount);
-        }
+                const fetched = await message.channel.fetchMessages({limit: deleteCount});
+                message.channel.bulkDelete(fetched)
+                    .catch(error => message.reply(`Je ne peux pas supprimer les messages car : ${error}`));
+            }
+        })
     },
 };
