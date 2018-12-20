3c11a0bc36a5a4e13fc76f2bd899dfee1a1c1bf0
Jordan RENARD <jord.renard@gmail.com>
2018-12-20 21:40:28 +0100

    Random game

--- commands/rg.js
+++ commands/rg.js
@@ -18,12 +18,42 @@ module.exports = {
 
         let Jordan = ["League of Legends", "Rocket League", "Brawlhalla", "H1Z1", "PUBG", "CS:Go", "Dofus", "Battlefield V", "Fifa", "Witch it", "Business Tour", "Minecraft", "Gang Beast", "Overcooked"];
         let Lowki = ["League of Legends", "Rocket League", "CS:Go", "Dofus", "Battlefield V", "Fifa"];
-        let Adrien = ["League of Legends", "Rocket League", "CS:Go", "Dofus", "Battlefield V", "Fifa"];
-        let Fiffou = ["League of Legends", "Rocket League", "CS:Go", "Dofus", "Battlefield V", "Fifa"];
-        let finalComparison = compare(Jordan, Lowki);
+        let Adrien = ["League of Legends"];
+        let Fiffou = ["Battlefield V"];
+
+        switch (args[0]) {
+            case 'Jordan':
+                args[0] = Jordan;
+                break;
+            case 'Lowki':
+                args[0] = Lowki;
+                break;
+            case 'Adrien':
+                args[0] = Adrien;
+                break;
+            case 'Fiffou':
+                args[0] = Fiffou;
+                break;
+        }
+
+        switch (args[1]) {
+            case 'Jordan':
+                args[1] = Jordan;
+                break;
+            case 'Lowki':
+                args[1] = Lowki;
+                break;
+            case 'Adrien':
+                args[1] = Adrien;
+                break;
+            case 'Fiffou':
+                args[1] = Fiffou;
+                break;
+        }
+
+        let finalComparison = compare(args[0], args[1]);
         let randomNumber = Math.floor(Math.random() * finalComparison.length);
 
-        // message.channel.send('Vous devez jouer à : ' + finalComparison[randomNumber]);
-        message.channel.send(typeof args[0]);
+        message.channel.send('Vous devez jouer à : ' + finalComparison[randomNumber]);
     },
 };
