9d8a965c6001bc7c90bcb468aba3182bdac42b0e
Jordan RENARD <jord.renard@gmail.com>
2018-12-30 23:11:35 +0100

    Refonte rg

---
+++ commands/rg2.js
@@ -0,0 +1,32 @@
+const fs = require("fs");
+
+module.exports = {
+    name: "rg2",
+    description: "Vous ne savez pas à quoi jouer ? Demandez !",
+    execute(message, args) {
+        /*function compare(arr1, arr2) {
+            const finalArray = [];
+            arr1.forEach((e1) => arr2.forEach((e2) => {
+                if (e1 === e2) {
+                    finalArray.push(e1);
+                }
+            }));
+
+            return finalArray;
+        }*/
+
+        if (args.length > 0) {
+            let arg0 = args[0].toUpperCase();
+            let arg1 = args[1].toUpperCase();
+            fs.readFile('./config/players_games.json', function (err, data) {
+                if (err) throw err;
+                message.channel.send(data.toString());
+            })
+        } else {
+            let games = ["League of Legends", "Rocket League", "H1Z1", "PUBG", "CS:Go", "Dofus", "Battlefield V", "Fifa 18", "DayZ", "Brawlhalla", "Minecraft", "Witch it", "Business Tour"];
+            let randomNumber = Math.floor(Math.random() * games.length);
+
+            message.channel.send('Vous devez jouer à : ' + games[randomNumber]);
+        }
+    },
+};
