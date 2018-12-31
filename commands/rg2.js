1243bd3a89775c9a7e96176e2e2bdd25db0612b0
Jordan RENARD <jord.renard@gmail.com>
2018-12-31 01:02:54 +0100

    Rg2

--- commands/rg2.js
+++ commands/rg2.js
@@ -16,17 +16,26 @@ module.exports = {
         }
 
         if (args.length > 0) {
-            let arg0 = args[0].toUpperCase();
-            let arg1 = args[1].toUpperCase();
-            fs.readFile('./config/players_games.json', function (err, data) {
-                if (err) throw err;
-                //message.channel.send(data.toString());
-                let content = data.toString();
-                let contentJsoned = JSON.parse(content);
-                let finalArray = compare(contentJsoned[arg0], contentJsoned[arg1]);
-                message.channel.send(finalArray.toString());
-                //message.channel.send(contentJsoned[arg1]);
-            })
+            if (args[0] == null || args[1] == null) {
+                message.channel.send("Je ne connais pas l'un des deux personnes");
+            } else {
+                let arg0 = args[0].toUpperCase();
+                let arg1 = args[1].toUpperCase();
+                let finalArray = null;
+                fs.readFile('./config/players_games.json', function (err, data) {
+                    if (err) throw err;
+                    //message.channel.send(data.toString());
+                    let content = data.toString();
+                    let contentJsoned = JSON.parse(content);
+                    finalArray = compare(contentJsoned[arg0], contentJsoned[arg1]);
+                    let randomNumber = Math.floor(Math.random() * finalArray.length);
+                    if (typeof finalArray[randomNumber] !== 'undefined') {
+                        message.channel.send('Vous devez jouer à : ' + finalArray[randomNumber]);
+                    } else {
+                        message.channel.send("Il n'y a pas de jeu en commun entre ces deux keum...");
+                    }
+                });
+            }
         } else {
             let games = ["League of Legends", "Rocket League", "H1Z1", "PUBG", "CS:Go", "Dofus", "Battlefield V", "Fifa 18", "DayZ", "Brawlhalla", "Minecraft", "Witch it", "Business Tour"];
             let randomNumber = Math.floor(Math.random() * games.length);
