ff9bb4e5bd40694077b2d8fed7a1d13b8709182f
Ikxa <jord.renard@gmail.com>
2019-01-08 00:14:13 +0100

    content

--- commands/content.js
+++ commands/content.js
@@ -1,19 +1,22 @@
 const fs = require("fs");
+let filename = './config/players_games.json';
 
 module.exports = {
     name: "content",
     description: "Vous avez un jeu qui n'est plus à jour ?",
     execute(message, args) {
         if (args.length > 0) {
-            fs.readFile('./config/players_games.json', function (err, data) {
+            fs.readFile(filename, function (err, data) {
                 if (err) throw err;
-                //message.channel.send(data.toString());
+                // TODO: Liste des jeux du mec passé en argument sous la forme JSON : contentJsoned[arg0]
                 let content = data.toString();
+                let arg0 = args[0].toUpperCase();
                 let contentJsoned = JSON.parse(content);
-                message.channel.send(contentJsoned["JORDAN"]);
-                message.channel.send(typeof contentJsoned);
-                // let contentToChange = contentJsoned[args[0]];
-                // message.channel.send(contentToChange);
+                contentJsoned[arg0] = "Battlefield V";
+                fs.writeFile(filename, JSON.stringify(content, null, 2), function (err) {
+                    if (err) return console.log(err);
+                    message.channel.send(contentJsoned[arg0]);
+                })
             });
         }
     },
