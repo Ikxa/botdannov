7ba4cf426c7f1c844162a36190251cc06857992d
Renard <jord.renard@gmail.com>
2019-01-07 03:42:12 +0100

    Afk

--- commands/afk.js
+++ commands/afk.js
@@ -5,6 +5,7 @@ module.exports = {
     description: "Tu comptes t'absenter, dis-le nous!",
     execute(message, args) {
         if (args.length > 0) {
+            let reason = args.join(" ");
             const client = new Client({
                 connectionString: process.env.DATABASE_URL,
                 ssl: true,
@@ -19,7 +20,7 @@ module.exports = {
                         message.channel.send('Vous avez déjà une raison d\'afk de préciser, merci de faire un .stopafk avant !');
                     } else {
                         client.query('insert into users_afk (id, nickname, reason, is_active) values ($1, $2, $3, 1)',
-                            [message.author.id, message.author.username, args[0]], (err) => {
+                            [message.author.id, message.author.username, reason], (err) => {
                                 if (err !== null && err !== '') console.log(err);
                                 message.channel.send('La raison de votre afk a bien été prise en compte');
                             });
