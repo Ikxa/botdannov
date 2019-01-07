5ffcd0b2e5fbb36816d6f785e0fde74b5cda33b2
= <jord.renard@gmail.com>
2019-01-08 00:00:34 +0100

    content

--- commands/content.js
+++ commands/content.js
@@ -10,9 +10,9 @@ module.exports = {
                 //message.channel.send(data.toString());
                 let content = data.toString();
                 let contentJsoned = JSON.parse(content);
-                message.channel.send(typeof content);
-                let contentToChange = contentJsoned[args[0]];
-                console.log(contentToChange);
+                message.channel.send(contentJsoned);
+                message.channel.send(typeof contentJsoned);
+                // let contentToChange = contentJsoned[args[0]];
                 // message.channel.send(contentToChange);
             });
         }
