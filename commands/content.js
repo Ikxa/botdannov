4cde074a5a41083c14829ba84aa2b22678ae853e
Renard <jord.renard@gmail.com>
2019-01-07 05:06:07 +0100

    Afk

---
+++ commands/content.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+const filename = './config/players_games.json';
+const file = require(filename);
+
+module.exports = {
+    name: "content",
+    description: "Vous avez un jeu qui n'est plus à jour ?",
+    execute(message) {
+        message.channel.send(file);
+
+        /*
+        file.key = "new value";
+        fs.writeFile(filename, JSON.stringify(file, null, 2), function (err) {
+            if (err) return console.log(err);
+            console.log(JSON.stringify(file));
+            console.log('writing to ' + filename);
+        });
+         */
+    },
+};
