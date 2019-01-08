743d75bad9c53e4e81e3017cfa5c4398178c7c14
Ikxa <jord.renard@gmail.com>
2019-01-08 03:42:34 +0100

    crudgames

---
+++ commands/rg2.js
@@ -0,0 +1,34 @@
+const {Client} = require('pg');
+
+module.exports = {
+    name: "rg2",
+    description: "Vous ne savez pas à quoi jouer ? Demandez !",
+    execute(message, args) {
+        function compare(arr1, arr2) {
+            const finalArray = [];
+            arr1.forEach((e1) => arr2.forEach((e2) => {
+                if (e1 === e2) {
+                    finalArray.push(e1);
+                }
+            }));
+
+            return finalArray;
+        }
+
+        if (args.length > 0) {
+            const client = new Client({
+                connectionString: process.env.DATABASE_URL,
+                ssl: true,
+            });
+
+            client.connect((err, client) => {
+                client.query('select list_of_games from players_games \
+                where nickname = $1', [args[0]], (err, result) => {
+                    if (err !== null && err !== '') console.log(err);
+                    const rows = result.rows;
+                    console.log(rows);
+                });
+            });
+        }
+    },
+};
