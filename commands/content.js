d7e31b7cb235fdb6aba256db2b13242260146cfe
Ikxa <jord.renard@gmail.com>
2019-01-08 01:52:34 +0100

    content

--- commands/content.js
+++ commands/content.js
@@ -15,7 +15,7 @@ module.exports = {
                 let newContent = "Battlefield V";
                 fs.writeFile(filename, JSON.stringify(newContent), function (err) {
                     if (err) return console.log(err);
-                    message.channel.send('Contenu modifié');
+                    message.channel.send('Contenu modifié!');
                 });
             });
         }
