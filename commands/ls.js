b5be06e61719151796b16bdceef8a63b8d3d71c3
Ikxa <jord.renard@gmail.com>
2019-01-09 07:09:19 +0100

    lastseen

--- commands/ls.js
+++ commands/ls.js
@@ -2,6 +2,6 @@ module.exports = {
     name: "ls",
     description: "Permet d'enregistrer la dernière heure à laquelle on vous a vu !",
     execute(message) {
-        message.channel.send('La dernière fois qu\'on a vu ' + message.author + ', c\'était à ' + (new Date().getTime()));
+        message.channel.send('La dernière fois qu\'on a vu ' + message.author + ', c\'était à ' + (new Date().getHours()) + ':' + (new Date().getMinutes()));
     },
 };
