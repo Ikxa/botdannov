d0ddd86e9b467965a580f9ffd5e86d2961b65920
Ikxa <jord.renard@gmail.com>
2019-01-09 07:24:26 +0100

    lastseen

--- commands/ls.js
+++ commands/ls.js
@@ -2,8 +2,14 @@ module.exports = {
     name: "ls",
     description: "Permet d'enregistrer la dernière heure à laquelle on vous a vu !",
     execute(message) {
-        let date = new Date();
-        message.channel.send('' + date.getUTCHours());
-        message.channel.send('La dernière fois qu\'on a vu ' + message.author + ', c\'était à ' + date.getHours() + 'h' + (new Date().getMinutes()));
+        let options = {
+            timeZone: 'Europe/Paris',
+            year: 'numeric', month: 'numeric', day: 'numeric',
+            hour: 'numeric', minute: 'numeric', second: 'numeric',
+        },
+        formatter = new Intl.DateTimeFormat([], options);
+        formatter.format(new Date());
+
+        message.channel.send(formatter.toString());
     },
 };
