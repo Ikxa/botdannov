aaaf83955ec8064806f50ee4ff208ecf0f6e23b3
Ikxa <jord.renard@gmail.com>
2019-01-13 06:27:35 +0100

    Survey

--- commands/survey.js
+++ commands/survey.js
@@ -3,11 +3,8 @@ module.exports = {
     description: "Lancez un sondage !",
     execute(message, args) {
         if (args.length > 0) {
-            let str = message.toString();
-            let args_array = str.split('|', args.length);
+            let args_array = args.filter(e => e !== '|');
             console.log(args_array);
-            console.log('Args');
-            console.log(args);
         }
     },
 };
