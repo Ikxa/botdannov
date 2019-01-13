5e20893d1c93187d6f23c0719603f59ac3d1ed7f
Ikxa <jord.renard@gmail.com>
2019-01-13 06:54:58 +0100

    Survey

--- commands/survey.js
+++ commands/survey.js
@@ -1,9 +1,12 @@
+const Discord = require("discord.js");
+const bot = new Discord.Client();
+
 module.exports = {
     name: "survey",
     description: "Lancez un sondage !",
     execute(message, args) {
         if (args.length > 0) {
-            const one = client.emojis.get("533885431247536129");
+            const one = bot.emojis.get("533885431247536129");
             let question = args.join(" ");
             let question_split = question.split('|');
             message.channel.send('**SONDAGE**: ' + question_split[0]);
