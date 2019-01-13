aaf9bbd02b90bf86524d884428d4040d4d2a0cee
Ikxa <jord.renard@gmail.com>
2019-01-13 06:43:49 +0100

    Survey

--- commands/survey.js
+++ commands/survey.js
@@ -4,7 +4,9 @@ module.exports = {
     execute(message, args) {
         if (args.length > 0) {
             let question = args.join(" ");
+            let question_split = question.split('|');
             console.log(question);
+            console.log(question_split);
             // message.channel.send('Sondage: ' + args_array[0])
         }
     },
