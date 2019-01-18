3d7f98b67c69e8ad7da7b970fc3fe8c76bc7366a
Ikxa <jord.renard@gmail.com>
2019-01-19 00:19:06 +0100

    DM

--- commands/debug.js
+++ commands/debug.js
@@ -2,13 +2,10 @@ module.exports = {
     name: "debug",
     description: "Vérifier les types",
     execute(message) {
-        console.log(message.member);
         let member = message.guild.fetchMember('193467165389619211')
         if (member !== null && typeof(member) !== 'undefined') {
-            message.channel.send('Bonjour Jordan!');
+            member.send('Bonjour Jordan!');
             console.log(member);
-        } else {
-            message.channel.send('Bfezhufzugrze');
         }
     },
 };
