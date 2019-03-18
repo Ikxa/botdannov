eeacdef605c6c37e073f2e328dcacfb43529ee2a
Jordan RENARD <jord.renard@gmail.com>
2019-03-18 23:25:27 +0100

    let's go

--- index.js
+++ index.js
@@ -1,106 +1,125 @@
-const Discord = require("discord.js");
-const fs = require("fs");
+const Discord = require('discord.js');
+const fs = require('fs');
 const bot = new Discord.Client();
 const maintenance = false;
 
-const {Client} = require('pg');
+const { Client } = require('pg');
 
 /* TODO : Cannot read property of undefined */
 const client = new Client({
-    connectionString: process.env.DATABASE_URL,
-    ssl: true,
+	connectionString: process.env.DATABASE_URL,
+	ssl: true
 });
-
 
 let prefix = '.';
 
 bot.commands = new Discord.Collection();
-const commandFiles = fs.readdirSync("./commands/");
+const commandFiles = fs.readdirSync('./commands/');
 
-commandFiles.forEach(file => {
-    const command = require(`./commands/${file}`); // eslint-disable-line
-    bot.commands.set(command.name, command);
+commandFiles.forEach((file) => {
+	const command = require(`./commands/${file}`); // eslint-disable-line
+	bot.commands.set(command.name, command);
 });
 
-bot.on("ready", () => {
-    bot.user.setActivity("Justin à poil", {type: "WATCHING"}).catch(err => console.error(err));
-    console.log("Bot ready");
-    // Database connection
-    client.connect((err, client) => {
-        // Create table for users_afk
-        client.query('create table if not exists users_afk( \
+bot.on('ready', () => {
+	bot.user.setActivity('Justin à poil', { type: 'WATCHING' }).catch((err) => console.error(err));
+	console.log('Bot ready');
+	// Database connection
+	client.connect((err, client) => {
+		// Create table for users_afk
+		client.query(
+			'create table if not exists users_afk( \
                 id text primary key, \
                 nickname text, \
                 reason text, \
-                is_active integer default 1)', (err, result) => {
-            //disconnent from database on error
-            if (err !== null && err !== '') console.log(err);
-        });
-    });
+                is_active integer default 1)',
+			(err, result) => {
+				//disconnent from database on error
+				if (err !== null && err !== '') console.log(err);
+			}
+		);
+	});
 });
 
-bot.on("disconnected", () => {
-    bot.login(process.env.TOKEN).catch(err => console.error(err));
+bot.on('disconnected', () => {
+	bot.login(process.env.TOKEN).catch((err) => console.error(err));
 });
 
-bot.on("message", message => {
-    // If message's author is a bot, just return and do nothing
-    if (message.author.bot) return;
+bot.on('message', (message) => {
+	// If message's author is a bot, just return and do nothing
+	if (message.author.id == 453121034988683265) {
+		return;
+	}
 
-    if (maintenance === true) {
-        // If maintenance is enabled, tell it and return
-        let already_said = 0;
-        if (already_said === 0) {
-            message.channel.send('Désolé, je suis en maintenance pour le moment.');
-            already_said = 1;
-        }
-        throw new Error("Mode maintenance activé sur le bot");
-    }
+	if (message.author.bot) {
+		message.delete();
+		message.channel.send("Le Chef des Bots ne t'autorise pas à parler !");
+	}
 
-    // TODO: Fonctionne correctement !
-    // id de l'utilisateur mentionné : **user_mentioned.id**
-    // résultat de la requête : **result.rows**
-    // rows = [ { reason: 'PauseCaca' } ]
-    // raison de l'afk : rows[0].reason
-    const user_mentioned = message.mentions.users.first();
-    if (typeof user_mentioned != "undefined") {
-        const client = new Client({
-            connectionString: process.env.DATABASE_URL,
-            ssl: true,
-        });
-        client.connect((err, client) => {
-            client.query('select reason from users_afk \
-                where is_active = 1 and id = $1', [user_mentioned.id], (err, result) => {
-                if (err !== null && err !== '') console.log(err);
-                const rows = result.rows;
-                if (typeof rows[0] !== 'undefined') {
-                    message.channel.send('Désolé, ' + user_mentioned + ' s\'est absenté pour la raison suivante : ' + rows[0].reason);
-                }
-            });
-        });
-    }
+	if (message.attachments.size > 0) {
+		message.delete();
+	}
 
-    /*if (message.attachments.size > 0) {
+	if (maintenance === true) {
+		// If maintenance is enabled, tell it and return
+		let already_said = 0;
+		if (already_said === 0) {
+			message.channel.send('Désolé, je suis en maintenance pour le moment.');
+			already_said = 1;
+		}
+		throw new Error('Mode maintenance activé sur le bot');
+	}
+
+	// TODO: Fonctionne correctement !
+	// id de l'utilisateur mentionné : **user_mentioned.id**
+	// résultat de la requête : **result.rows**
+	// rows = [ { reason: 'PauseCaca' } ]
+	// raison de l'afk : rows[0].reason
+	const user_mentioned = message.mentions.users.first();
+	if (typeof user_mentioned != 'undefined') {
+		const client = new Client({
+			connectionString: process.env.DATABASE_URL,
+			ssl: true
+		});
+		client.connect((err, client) => {
+			client.query(
+				'select reason from users_afk \
+                where is_active = 1 and id = $1',
+				[ user_mentioned.id ],
+				(err, result) => {
+					if (err !== null && err !== '') console.log(err);
+					const rows = result.rows;
+					if (typeof rows[0] !== 'undefined') {
+						message.channel.send(
+							'Désolé, ' + user_mentioned + " s'est absenté pour la raison suivante : " + rows[0].reason
+						);
+					}
+				}
+			);
+		});
+	}
+
+	/*if (message.attachments.size > 0) {
         message.channel.send('Vous avez envoyé une photo');
         let member = ;
         member.send(message.attachment)
     }*/
 
-    // Command execution
-    if (message.content.startsWith(prefix)) {
-        const args = message.content.slice(prefix.length).split(" ");
-        const commandName = args.shift().toLowerCase();
+	// Command execution
+	if (message.content.startsWith(prefix)) {
+		const args = message.content.slice(prefix.length).split(' ');
+		const commandName = args.shift().toLowerCase();
 
-        if (!bot.commands.has(commandName)) return;
+		if (!bot.commands.has(commandName)) return;
 
-        const command = bot.commands.get(commandName);
-        try {
-            command.execute(message, args, bot);
-        } catch (error) {
-            console.error(error);
-            message.reply("Error code 3 : Execute command");
-        }
-    }
+		const command = bot.commands.get(commandName);
+		try {
+			command.execute(message, args, bot);
+		} catch (error) {
+			console.error(error);
+			message.reply('Error code 3 : Execute command');
+		}
+	}
 });
 
 bot.login(process.env.TOKEN);
