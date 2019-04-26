2cf64d9d199c27fc18155881cbce5417174b987a
= <jord.renard@gmail.com>
2019-04-26 03:51:56 +0200

    vocal

--- commands/vocal.js
+++ commands/vocal.js
@@ -4,26 +4,19 @@ module.exports = {
 	name        : 'vocal',
 	description : 'Un max de barre !',
 	execute(message, args) {
-		if (message.content == '!vocal') {
-			let voiceChannel = message.guild.channels
-				.filter(function(channel) {
-					return channel.type === 'voice';
-				})
-				.last();
+		let voiceChannel = message.guild.channels
+			.filter(function(channel) {
+				return channel.type === 'voice';
+			})
+			.last();
 
-			voiceChannel
-				.join()
-				.then((connection) => {
-					const dispatcher = connection.playFile('./sounds/louane.mp3');
-					if (args[0] === 'pause') dispatcher.pause();
-					if (args[0] === 'resume') dispatcher.resume();
-					dispatcher.on('end', (end) => {
-						voiceChannel.leave();
-					});
-				})
-				.catch(console.error);
-		} else {
-			message.channel.send('CA MARCHE PAS!');
-		}
+		voiceChannel.join().then((connection) => {
+			const dispatcher = connection.playFile('./sounds/louane.mp3');
+			if (args[0] === 'pause') dispatcher.pause();
+			if (args[0] === 'resume') dispatcher.resume();
+			dispatcher.on('end', (end) => {
+				voiceChannel.leave();
+			});
+		});
 	}
 };
