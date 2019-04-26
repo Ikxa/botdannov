b4a48ea1190dfd9abb1612e4266a764cdbcea976
= <jord.renard@gmail.com>
2019-04-26 18:03:23 +0200

    vocal

--- commands/vocal.js
+++ commands/vocal.js
@@ -15,19 +15,13 @@ module.exports = {
 		voiceChannel.join().then((connection) => {
 			//const dispatcher = connection.playFile('./sounds/louane.mp3');
 			const stream = ytdl(args[0], { filter: 'audioonly' });
-			if (args[0] != 'stop') {
-				const dispatcher = connection.playStream(stream, playingOptions);
-			} else {
-				voiceChannel.disconnect();
-				dispatcher.end();
-			}
+			const dispatcher = connection.playStream(stream, playingOptions);
 
 			if (args[0] === 'pause') dispatcher.pause();
 			if (args[0] === 'resume') dispatcher.resume();
-			if (args[0])
-				dispatcher.on('end', (end) => {
-					voiceChannel.leave();
-				});
+			dispatcher.on('end', (end) => {
+				voiceChannel.leave();
+			});
 		});
 	}
 };
