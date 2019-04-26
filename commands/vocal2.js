61ee3cd2e698fe3562308f30e1e43069a7e96462
= <jord.renard@gmail.com>
2019-04-26 16:20:21 +0200

    vocal

--- commands/vocal2.js
+++ commands/vocal2.js
@@ -1,18 +1,9 @@
 const fs = require('fs');
 
 module.exports = {
-	name        : 'vocal',
+	name        : 'vocal2',
 	description : 'Un max de barre !',
 	execute(message, args) {
-		const contentFolder = fs.readdir('./sounds');
-		var contentFolderName = [];
-		contentFolder.forEach((file) => {
-			const soundName = file.substring(0, file.length - 4);
-			contentFolderName = contentFolderName.push(soundName);
-		});
-
-		message.channel.send(contentFolderName);
-
 		let voiceChannel = message.guild.channels
 			.filter(function(channel) {
 				return channel.type === 'voice';
@@ -23,9 +14,9 @@ module.exports = {
 			const dispatcher = connection.playFile('./sounds/louane.mp3');
 			if (args[0] === 'pause') dispatcher.pause();
 			if (args[0] === 'resume') dispatcher.resume();
-			dispatcher.on('end', (end) => {
+			/*dispatcher.on('end', (end) => {
 				voiceChannel.leave();
-			});
+			});*/
 		});
 	}
 };
