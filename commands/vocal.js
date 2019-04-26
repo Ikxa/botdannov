2fd79bd60670923db1bceb369ca8c17fc38bfbe6
= <jord.renard@gmail.com>
2019-04-27 01:15:05 +0200

    vocal

--- commands/vocal.js
+++ commands/vocal.js
@@ -18,7 +18,13 @@ module.exports = {
 			const dispatcher = connection.playStream(stream, playingOptions);
 
 			if (args[0] === 'pause') dispatcher.pause();
-			if (args[0] === 'resume') dispatcher.resume();
+
+			if (args[0] === 'stop') {
+				dispatcher.end();
+			} else {
+				message.channel.send("Arg0 n'est pas stop");
+			}
+
 			dispatcher.on('end', (end) => {
 				voiceChannel.leave();
 			});
