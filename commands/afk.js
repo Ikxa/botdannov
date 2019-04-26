97a76398ddd61193c64795d060337bf458cdbbe0
= <jord.renard@gmail.com>
2019-04-26 14:08:42 +0200

    afk and vocal

--- commands/afk.js
+++ commands/afk.js
@@ -11,33 +11,35 @@ module.exports = {
 				ssl              : true
 			});
 
-			client.connect((err, client) => {
-				client.query(
-					'select reason from users_afk \
+			if (reason != 'stop') {
+				client.connect((err, client) => {
+					client.query(
+						'select reason from users_afk \
                 where is_active = 1 and id = $1',
-					[ message.author.id ],
-					(err, result) => {
-						if (err !== null && err !== '') console.log(err);
-						const rows = result.rows;
-						if (typeof rows[0] !== 'undefined') {
-							message.channel.send(
-								"Vous avez déjà une raison d'afk de préciser, merci de faire un .stopafk avant !"
-							);
-						} else {
-							client.query(
-								'insert into users_afk (id, nickname, reason, is_active) values ($1, $2, $3, 1)',
-								[ message.author.id, message.author.username, reason ],
-								(err) => {
-									if (err !== null && err !== '') console.log(err);
-									message.channel.send('La raison de votre afk a bien été prise en compte');
-								}
-							);
+						[ message.author.id ],
+						(err, result) => {
+							if (err !== null && err !== '') console.log(err);
+							const rows = result.rows;
+							if (typeof rows[0] !== 'undefined') {
+								message.channel.send(
+									"Vous avez déjà une raison d'afk de préciser, merci de faire un .stopafk avant !"
+								);
+							} else {
+								client.query(
+									'insert into users_afk (id, nickname, reason, is_active) values ($1, $2, $3, 1)',
+									[ message.author.id, message.author.username, reason ],
+									(err) => {
+										if (err !== null && err !== '') console.log(err);
+										message.channel.send('La raison de votre afk a bien été prise en compte');
+									}
+								);
+							}
 						}
-					}
-				);
-			});
+					);
+				});
+			}
 
-			if (args[0] === 'stop') {
+			if (reason == 'stop') {
 				client.connect((err, client) => {
 					client.query(
 						'delete from users_afk where nickname = $1 and is_active = 1',
