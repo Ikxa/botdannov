201e811bfa8d15cd75e232daa59edb84f4a8ffe8
= <jord.renard@gmail.com>
2019-04-26 02:24:46 +0200

    vocal

--- commands/vocal.js
+++ commands/vocal.js
@@ -1,22 +1,26 @@
 module.exports = {
 	name        : 'vocal',
 	description : 'Un max de barre !',
-	execute() {
-		let voiceChannel = message.guild.channels
-			.filter(function(channel) {
-				return channel.type === 'voice';
-			})
-			.last();
+	execute(message) {
+		if (message.content == '.vocal') {
+			let voiceChannel = message.guild.channels
+				.filter(function(channel) {
+					return channel.type === 'voice';
+				})
+				.last();
 
-		voiceChannel
-			.join()
-			.then(function(connection) {
-				fs.readdir('../sounds/', function(err, items) {
-					for (let i = 0; i < items.length; i++) {
-						const dispatcher = connection.playFile('../sounds/' + items[i]);
-					}
-				});
-			})
-			.catch(console.error);
+			voiceChannel
+				.join()
+				.then(function(connection) {
+					fs.readdir('../sounds/', function(err, items) {
+						for (let i = 0; i < items.length; i++) {
+							const dispatcher = connection.playFile('../sounds/' + items[i]);
+						}
+					});
+				})
+				.catch(console.error);
+		} else {
+			message.send('CA MARCHE PAS!');
+		}
 	}
 };
