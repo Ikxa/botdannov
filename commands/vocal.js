489a101103fd4f80e50e89a2cdd303058b973849
= <jord.renard@gmail.com>
2019-04-26 02:13:16 +0200

    vocal 2

--- commands/vocal.js
+++ commands/vocal.js
@@ -4,17 +4,21 @@ module.exports = {
 	name        : 'vocal',
 	description : 'Un max de barre !',
 	execute() {
-		var isReady = true;
-		if (isReady && message.content === 'vocal') {
-			isReady = false;
-			var voiceChannel = message.member.voiceChannel;
-			voiceChannel
-				.join()
-				.then((connection) => {
-					const dispatcher = connection.playFile('../sounds/lacoupe.mp3');
-					dispatcher.on('end', (end) => voiceChannel.leave());
-				})
-				.catch((err) => message.send(err));
-		}
+		let voiceChannel = message.guild.channels
+			.filter(function(channel) {
+				return channel.type === 'voice';
+			})
+			.last();
+
+		voiceChannel
+			.join()
+			.then(function(connection) {
+				fs.readdir('./music', function(err, items) {
+					for (let i = 0; i < items.length; i++) {
+						const dispatcher = connection.playFile('../sounds/' + items[i]);
+					}
+				});
+			})
+			.catch(console.error);
 	}
 };
