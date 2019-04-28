ad5b2069418136a5e733a3b8371bfa3b9c796d90
Jordan RENARD <jord.renard@gmail.com>
2019-04-28 14:13:35 +0200

    vocal

--- commands/vocal.js
+++ commands/vocal.js
@@ -12,26 +12,31 @@ module.exports = {
 			})
 			.last();
 
-		voiceChannel.join().then((connection) => {
-			//const dispatcher = connection.playFile('./sounds/louane.mp3');
-			const stream = ytdl(args[0], { filter: 'audioonly' });
-			const dispatcher = connection.playStream(stream, playingOptions);
+		voiceChannel
+			.join()
+			.then((connection) => {
+				//const dispatcher = connection.playFile('./sounds/louane.mp3');
+				const stream = ytdl(args[0], { filter: 'audioonly' });
+				const dispatcher = connection.playStream(stream, playingOptions);
 
-			if (args[0] === 'pause') dispatcher.pause();
+				if (args[0] === 'pause') dispatcher.pause();
 
-			if (args[0] === 'stop') {
-				dispatcher.stream.end();
-			} else {
-				message.channel.send("Arg0 n'est pas stop");
-			}
+				if (args[0] === 'stop') {
+					dispatcher.stream.end();
+				} else {
+					message.channel.send("Arg0 n'est pas stop");
+				}
 
-			dispatcher.on('error', (err) => {
-				message.channel.send(err);
+				dispatcher.on('error', (err) => {
+					message.channel.send(err);
+				});
+
+				dispatcher.on('end', (end) => {
+					voiceChannel.leave();
+				});
+			})
+			.catch((e) => {
+				console.log(e);
 			});
-
-			dispatcher.on('end', (end) => {
-				voiceChannel.leave();
-			});
-		});
 	}
 };
