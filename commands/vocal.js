eb8fbf2649fb0724cd354351e0be86b16853bafc
= <jord.renard@gmail.com>
2019-04-29 21:25:31 +0200

    vocal stop

--- commands/vocal.js
+++ commands/vocal.js
@@ -2,8 +2,8 @@ const fs = require('fs');
 const ytdl = require('ytdl-core');
 
 module.exports = {
-	name: 'vocal',
-	description: 'Un max de barre !',
+	name        : 'vocal',
+	description : 'Un max de barre !',
 	execute(message, args) {
 		const playingOptions = { filter: 'audioonly', bitrate: 192000 };
 		console.log(args[0]);
@@ -17,23 +17,8 @@ module.exports = {
 		voiceChannel
 			.join()
 			.then((connection) => {
-				if (args[0] != 'stop') {
-					const stream = ytdl(args[0], { filter: 'audioonly' });
-					const dispatcher = connection.playStream(stream, playingOptions);
-				} else {
-					const receiver = connection.createReceiver();
-					const audioStream = receiver.createStream();
-					audioStream.end();
-					// dispatcher.stream.end();
-				}
-
-				/*if (args[0] == 'pause') dispatcher.pause();
-
-				if (args[0] == 'stop') {
-					dispatcher.stream.end();
-				} else {
-					message.channel.send("Arg0 n'est pas stop");
-				}*/
+				const stream = ytdl(args[0], { filter: 'audioonly' });
+				const dispatcher = connection.playStream(stream, playingOptions);
 
 				dispatcher.on('error', (err) => {
 					message.channel.send(err);
