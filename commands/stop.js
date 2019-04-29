d986e0faefdffdc1c80956bfd5d580ec7d440c1e
= <jord.renard@gmail.com>
2019-04-29 20:59:50 +0200

    vocal stop

--- commands/stop.js
+++ commands/stop.js
@@ -1,11 +1,6 @@
-const fs = require('fs');
-const ytdl = require('ytdl-core');
-
-module.exports = {
-	name: 'stop',
-	description: 'Un max de barre !',
-	execute(message, args) {
-		message.guild.me.voiceChannel.leave();
-		message.channel.send("Je m'en vais!");
-	}
-};
+export const name = 'stop';
+export const description = 'Stopper la musique !';
+export function execute(message, args) {
+	message.guild.me.voiceChannel.leave();
+	message.channel.send("J'ai dead ça!");
+}
