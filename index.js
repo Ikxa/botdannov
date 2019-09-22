4f92d8f1d882a944f366c5caac6afd431c49af28
Jordan Renard <jord.renard@gmail.com>
2019-09-22 14:19:21 +0200

    counter message

--- index.js
+++ index.js
@@ -68,7 +68,7 @@ bot.on('disconnected', () => {
 });
 
 bot.on('message', (message) => {
-	message.channel.sendMessage(message.author);
+	message.reply(message.author);
 
 	const thisWord = 'fart';
 	if (message.content.includes(thisWord)) {
