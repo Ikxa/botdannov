bcb283b66938eacd1a1ab634c64aa47c7d94b061
Jordan Renard <jord.renard@gmail.com>
2019-09-22 22:57:39 +0200

    counter msg

--- index.js
+++ index.js
@@ -111,6 +111,7 @@ bot.on('message', (message) => {
     }
 
     if (message.author.id == 453121034988683265 || message.author.id == 231451354470678528) {
+        message.delete(1000);
         return;
     }
 
