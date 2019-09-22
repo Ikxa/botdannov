79bc151dbb25016da3495837f654493a26922dd1
Jordan Renard <jord.renard@gmail.com>
2019-09-22 20:56:57 +0200

    blagoune

--- commands/blagoune.js
+++ commands/blagoune.js
@@ -5,6 +5,7 @@ module.exports = {
     description: "Un max de barre !",
     execute(message) {
         axios.get("http://api.yomomma.info/").then(response => {
+            console.log(response.joke);
             message.channel.send(response.joke);
         });
     },
