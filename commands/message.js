6f5801e8d5adc6d8cd9fb4adc4b49f520f873581
Jordan Renard <jord.renard@gmail.com>
2019-09-22 20:00:32 +0200

    counter msg

--- commands/message.js
+++ commands/message.js
@@ -1,26 +1,28 @@
-const { Client } = require('pg');
+const {Client} = require('pg');
 
 module.exports = {
-    name        : 'message',
-    description : "Comment participes-tu sur le serveur ?",
+    name: 'message',
+    description: "Comment participes-tu sur le serveur ?",
     execute(message, args) {
         if (!message.author.bot) {
             const client = new Client({
-                connectionString : process.env.DATABASE_URL,
-                ssl              : true
+                connectionString: process.env.DATABASE_URL,
+                ssl: true
             });
             message.channel.send('Je calcule combien de messages vous avez envoyé');
-            client.query(
-                'select id, nickname, nb from counter_msg \
-                where nickname = $1',
-                [message.author.username.toString()],
-                (err, result) => {
-                    if (err !== null && err !== '') message.channel.send(err);
-                    const rows = result.rows;
-                    message.channel.send('Vous avez envoyé ' + rows[0]['nb'] + ' message(s) sur le serveur.');
-                }
-            );
-
+            client.connect((err, client) => {
+                client.query(
+                    'select id, nickname, nb from counter_msg \
+                    where nickname = $1',
+                    [message.author.username.toString()],
+                    (err, result) => {
+                        if (err !== null && err !== '') console.log(err);
+                        const rows = result.rows;
+                        console.log(result);
+                        message.channel.send('Vous avez envoyé ' + rows[0]['nb'] + ' message(s) sur le serveur.');
+                    }
+                );
+            });
         }
     }
 };
