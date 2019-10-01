9c3776c71765e89aab93930aada4c35692f06db0
Jordan Renard <jord.renard@gmail.com>
2019-10-02 01:15:57 +0200

    blagoune

--- commands/blagoune.js
+++ commands/blagoune.js
@@ -3,10 +3,12 @@ const axios = require("axios");
 module.exports = {
     name: "blagoune",
     description: "Un max de barre !",
-    execute(message) {
+    execute(message, args) {
+        console.log("J'essaye de faire la requête axios");
         axios.get("http://api.yomomma.info/").then(response => {
             console.log(response);
             message.channel.send(response.joke);
         });
+        console.log("Je suis censé l'avoir réussi !");
     },
 };
