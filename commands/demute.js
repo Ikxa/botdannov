2141b55b119a3ccea2157fb162dc84f75c3b2610
Jordan RENARD <jord.renard@gmail.com>
2019-10-02 19:16:23 +0200

    demute

---
+++ commands/demute.js
@@ -0,0 +1,24 @@
+const {Client} = require('pg');
+
+module.exports = {
+    name: 'demute',
+    description: "Demute tout le monde!",
+    execute(message, args) {
+        if (!message.author.bot) {
+            const client = new Client({
+                connectionString: process.env.DATABASE_URL,
+                ssl: true
+            });
+
+            client.connect((err, client) => {
+                client.query(
+                    'delete * from mute_table',
+                    (err) => {
+                        if (err !== null && err !== '') console.log(err);
+                    }
+                );
+                message.channel.send("Tout le monde est demute");
+            });
+        }
+    }
+};
