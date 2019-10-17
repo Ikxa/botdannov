ba774c28568aa77976ff72c94a07db2f618898f9
Jordan Renard <jord.renard@gmail.com>
2019-10-18 01:12:36 +0200

    vocal

--- commands/vocal.js
+++ commands/vocal.js
@@ -2,39 +2,37 @@ const fs = require('fs');
 const ytdl = require('ytdl-core');
 
 module.exports = {
-	name        : 'vocal',
-	description : 'Un max de barre !',
-	execute(message, args) {
-		const playingOptions = { filter: 'audioonly', bitrate: 192000 };
+    name: 'vocal',
+    description: 'Un max de barre !',
+    execute(message, args) {
+        const playingOptions = {filter: 'audioonly', bitrate: 192000};
 
-		console.log(args[0]);
+        console.log(args[0]);
 
-		let voiceChannel = message.guild.channels
-			.filter(function(channel) {
-				return channel.type === 'voice';
-			})
-			.last();
+        let voiceChannel = message.member.voiceChannel;
+        let connection = voiceChannel.join();
 
-		voiceChannel
-			.join()
-			.then((connection) => {
-				console.log("Je commence !!");
+        const dispatcher = connection.playStream(ytdl(args[0], {filter: 'audioonly'}), playingOptions);
 
-				const stream = ytdl(args[0], { filter: 'audioonly' });
-				const dispatcher = connection.playStream(stream, playingOptions);
+        dispatcher.on('error', (err) => {
+            message.channel.send(err);
+        });
 
-				dispatcher.on('error', (err) => {
-					message.channel.send(err);
-				});
+        dispatcher.on('end', (end) => {
+            voiceChannel.leave();
+        });
 
-				dispatcher.on('end', (end) => {
-					voiceChannel.leave();
-				});
+        /*voiceChannel
+            .join()
+            .then((connection) => {
+                console.log("Je commence !!");
 
-				console.log("J'ai fini, je quitte !");
-			})
-			.catch((e) => {
-				console.log(e);
-			});
-	}
+
+
+                console.log("J'ai fini, je quitte !");
+            })
+            .catch((e) => {
+                console.log(e);
+            });*/
+    }
 };
