656e77788e4053c9f45283b4192e5cc34506e87f
Jordan Renard <jord.renard@gmail.com>
2019-10-25 05:53:16 +0200

    momma

--- commands/momma.js
+++ commands/momma.js
@@ -4,6 +4,7 @@ module.exports = {
     name: 'momma',
     description: "Ta mère est tellement... ",
     execute(message, args) {
+        console.log("J'essaie de récupérer la blague!");
         axios.get('https://yomomma.info/')
             .then(function (response) {
                 // handle success
