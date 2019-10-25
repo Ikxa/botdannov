0d76a7b4600b6163db8b9573c080124e69397ddc
Jordan Renard <jord.renard@gmail.com>
2019-10-25 04:43:29 +0200

    message

--- commands/message.js
+++ commands/message.js
@@ -17,10 +17,15 @@ module.exports = {
                     (err, result) => {
                         if (err !== null && err !== '') console.log(err);
                         const rows = result.rows;
-                        if (rows[0]['nb'] > 1)
-                            message.channel.send('Vous avez envoyé ' + rows[0]['nb'] + ' messages sur le serveur.');
-                        else
-                            message.channel.send('Vous avez envoyé ' + rows[0]['nb'] + ' message sur le serveur.');
+                        if (rows[0]['nb'] > 0) {
+                            if (rows[0]['nb'] > 1)
+                                message.channel.send('Vous avez envoyé ' + rows[0]['nb'] + ' messages sur le serveur.');
+                            else
+                                message.channel.send('Vous avez envoyé ' + rows[0]['nb'] + ' message sur le serveur.');
+                        } else {
+                            message.channel.send("Vous n'avez pas encore envoyé de message sur le serveur !");
+                        }
+
 
                     }
                 );
