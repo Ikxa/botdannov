f370b0c2a1a5f518e39282ed0ce3a997c4c26eb3
Jordan Renard <jord.renard@gmail.com>
2019-10-26 04:05:46 +0200

    UTCMinutes

--- index.js
+++ index.js
@@ -92,7 +92,7 @@ function verifyMuted() {
                     is_muted = true;
                     console.log("Quelqu'un est mute");
                     let mutedTime = rows[0]['muted_at'];
-                    let currentTime = new Date().toUTCString();
+                    let currentTime = new Date().getUTCMinutes();
                     console.log(mutedTime);
                     console.log(currentTime);
                 } else {
