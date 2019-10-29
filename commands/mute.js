15a5b483aeed9a02e35cae44e4546924ddc159ce
Jordan Renard <jord.renard@gmail.com>
2019-10-29 03:29:22 +0100

    today

--- commands/mute.js
+++ commands/mute.js
@@ -10,12 +10,13 @@ module.exports = {
                 connectionString: process.env.DATABASE_URL,
                 ssl: true
             });
-            let today = moment().format('YYYY-MM-DD HH:mm:ss');
-            let todayParsed = moment(today);
-            let todayDate = moment(todayParsed['_i']).format('YYYY-MM-DD HH:mm:ss');
-            console.log('todayDate');
-            console.log('typeof todayDate ' + typeof todayDate);
-            console.log(todayDate);
+
+            let today = moment().toDate();
+            console.log("today's date");
+            console.log(today);
+            console.log("today's typeof");
+            console.log(typeof today);
+
             client.connect((err, client) => {
                 client.query(
                     'insert into mute_table (id, nickname, muted_at) values ($1, $2, $3)',
