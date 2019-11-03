b4144848dd71631fe7e7e18a0f4c86450e816b46
Jordan Renard <jord.renard@gmail.com>
2019-11-03 05:30:32 +0100

    poll

--- commands/poll.js
+++ commands/poll.js
@@ -4,18 +4,28 @@ module.exports = {
     name: "poll",
     description: "Lancez un sondage !",
     execute(message, args) {
-        // const emoji = message.guild.emojis.find(emoji => emoji.name === 'ayy');
-        // message.react(emoji);
         let choices = [];
         let fullPoll = args.join(' ');
         let question = fullPoll.split("/")[0];
         let split = fullPoll.split("/");
+        let reactions = {1: "one", 2: "two", 3: "three", 4: "four"};
         for (let i = 1; i <= (split.length - 1); i++) {
             choices.push(split[i].replace( /\s/g, ''));
         }
-        console.log("question :");
-        console.log(question);
-        console.log("choix :");
-        console.log(choices);
+
+        message.channel.send(question);
+        choices.forEach(function (c) {
+            message.channel.send(c.toString());
+        });
+
+        for (let i = 0; i <= (choices.length - 1); i++) {
+            for (let key in reactions) {
+                if (key == i) {
+                    console.log("key " + i + " has value " + reactions[i]);
+                    const emoji = message.guild.emojis.find(emoji => emoji.name === reactions[i]);
+                    message.react(emoji);
+                }
+            }
+        }
     },
 };
