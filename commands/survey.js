99b8dd6777ab53553a32c8e068fe92a037e48aeb
Jordan Renard <jord.renard@gmail.com>
2019-11-03 03:51:19 +0100

    survey 4

--- commands/survey.js
+++ commands/survey.js
@@ -4,8 +4,7 @@ module.exports = {
     execute(message, args) {
         // const emoji = message.guild.emojis.find(emoji => emoji.name === 'ayy');
         // message.react(emoji);
-        args.forEach(function (e) {
-            console.log(e);
-        });
+        const argsJoined = args.join(" ");
+        console.log(argsJoined);
     },
 };
