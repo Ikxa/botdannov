86aba54c26f7cf73a71a432c236f01737fba48c3
Jordan Renard <jord.renard@gmail.com>
2019-11-03 04:45:51 +0100

    poll

--- commands/poll.js
+++ commands/poll.js
@@ -6,16 +6,8 @@ module.exports = {
     execute(message, args) {
         // const emoji = message.guild.emojis.find(emoji => emoji.name === 'ayy');
         // message.react(emoji);
-        const embed = new Discord.MessageEmbed()
-            .setColor(0xffffff)
-            .setFooter('Votez !')
-            .setDescription(args.join(' '))
-            .setTitle('Vote créé par ${message.author.username.toString()}');
-
-        let msg = message.channel.send(embed);
-        msg.react('smile');
-        msg.react('joy');
-
-        message.delete({timeout: 1000});
+        let fullPoll = args.join(' ');
+        let question = fullPoll.split("/")[0];
+        console.log(question);
     },
 };
