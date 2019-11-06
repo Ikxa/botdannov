da4ac7077bd3ae0f280dc2df77809db9d6d31f76
Jordan Renard <jord.renard@gmail.com>
2019-11-06 13:03:52 +0100

    rn

--- commands/rn.js
+++ commands/rn.js
@@ -3,8 +3,12 @@ module.exports = {
     description: "!rn limit - pour obtenir un nombre aléatoire entre 0 et votre limite",
     execute(message, args) {
         if (args.length > 0) {
-            const number = Math.floor((Math.random() * args[0]) + 1);
-            message.channel.send('Numéro aléatoire : ' + number);
+            if (args[0] != 0) {
+                const number = Math.floor((Math.random() * args[0]) + 1);
+                message.channel.send('Numéro aléatoire : ' + number);
+            } else {
+                message.channel.send("CONSANGUIN VILETTE");
+            }
         }
     },
 };
