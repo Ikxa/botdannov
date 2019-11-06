3324de4c11969ee32c549312eaa3e4fd6996e6b8
Jordan Renard <jord.renard@gmail.com>
2019-11-06 05:41:56 +0100

    embed

--- commands/poll.js
+++ commands/poll.js
@@ -17,35 +17,16 @@ module.exports = {
         const embed = new Discord.RichEmbed()
             .setTitle("Sondage :")
             .setAuthor(message.author.username.toString(), message.author.avatarURL)
-            /*
-             * Alternatively, use "#00AE86", [0, 174, 134] or an integer number.
-             */
             .setColor(0x00AE86)
             .setDescription(question)
-            // .setFooter("This is the footer text, it can hold 2048 characters", "http://i.imgur.com/w1vhFSR.png")
-            // .setImage("http://i.imgur.com/yVpymuV.png")
-            // .setThumbnail("http://i.imgur.com/p2qNFag.png")
-            // .setTimestamp()
-            // .setURL("https://discord.js.org/#/docs/main/indev/class/RichEmbed")
-
-            // .addField("Inline Field", "They can also be inline.", true)
             .addBlankField(true);
-            // .addField("Inline Field 3", "You can have a maximum of 25 fields.", true);
 
+        let cpt = 0;
         choices.forEach(function (c) {
-            embed.addField(c.toString(), c.toString(), true)
+            cpt = cpt + 1;
+            embed.addField('Choix ' + cpt + ' :', c.toString())
         });
 
         message.channel.send({embed});
-
-        /*for (let i = 0; i <= (choices.length - 1); i++) {
-            for (let key in reactions) {
-                if (key == i) {
-                    console.log("key " + i + " has value " + reactions[i]);
-                    // const emoji = bot.emojis.find(emoji => emoji.name === reactions[i].toString());
-                    message.react("640414254423605267");
-                }
-            }
-        }*/
     },
 };
