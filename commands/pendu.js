ff1134cc01c00df74f23a34de330b5814a89feb3
Jordan Renard <jord.renard@gmail.com>
2019-11-20 15:34:05 +0100

    pendu

---
+++ commands/pendu.js
@@ -0,0 +1,18 @@
+const {Client} = require('pg');
+const fs = require("fs");
+
+module.exports = {
+    name: 'message',
+    description: "Comment participes-tu sur le serveur ?",
+    execute(message, args) {
+        if (args[0] === 'start') {
+            fs.readFile('./config/mot_dictionnaire.txt', function (err, data) {
+                if (err) throw err;
+
+                let content = data.toString();
+                // let contentJsoned = JSON.parse(content);
+                console.log(content);
+            });
+        }
+    }
+};
