022441707e98f78a24e111b9029ae286ccae22b8
Jordan Renard <jord.renard@gmail.com>
2019-11-20 21:31:29 +0100

    pendu

--- commands/pendu.js
+++ commands/pendu.js
@@ -9,23 +9,31 @@ module.exports = {
             let file = './config/mots_dictionnaire.txt';
             let countLines = 0;
             let arrayOfLines = [];
+            let randomWord = '';
+            let randomWordHide = '';
+            let lives = 5;
             let rl = readline.createInterface({
                 input: fs.createReadStream(file),
                 output: process.stdout,
                 terminal: false
             });
+
             rl.on('line', function (line) {
                 countLines++;
                 arrayOfLines.push(line);
             });
+
             rl.on('close', function () {
                 // console.log(countLines);
                 let randomNumber = Math.floor(Math.random() * Math.floor(countLines));
-                console.log('Nombre de lignes : ' + countLines);
-                console.log(arrayOfLines);
-                console.log('Numéro aléatoire : ' + randomNumber);
-                console.log('Mot aléatoire : ' + arrayOfLines[randomNumber]);
+                randomWord = arrayOfLines[randomNumber].toLowerCase();
             });
+
+            for (let i = 0; i <= randomWord.length; i++) {
+                randomWordHide += '_';
+            }
+
+            console.log(randomWordHide);
         }
     }
 };
