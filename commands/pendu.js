d143aa20d5463e0d4a35e2669e481125f929d1af
Jordan Renard <jord.renard@gmail.com>
2019-11-20 15:43:24 +0100

    pendu

--- commands/pendu.js
+++ commands/pendu.js
@@ -5,10 +5,12 @@ module.exports = {
     name: 'message',
     description: "Comment participes-tu sur le serveur ?",
     execute(message, args) {
+        console.log('Salut 1');
         if (args[0] === 'start') {
+            console.log('Salut 2');
             fs.readFile('./config/mots_dictionnaire.txt', function (err, data) {
                 if (err) throw err;
-
+                console.log('Salut 3');
                 let content = data.toString();
                 // let contentJsoned = JSON.parse(content);
                 console.log(content);
