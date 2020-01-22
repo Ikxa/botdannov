18704a75f2dbbe8d3d460f5ecb5585f83a685703
EidorianP <60190184+EidorianP@users.noreply.github.com>
2020-01-22 18:27:39 +0100

    Add files via upload

--- commands/RandomOpening.js
+++ commands/RandomOpening.js
@@ -5,6 +5,5 @@ module.exports = {
       var facts = ["Jordan sort son fou et Adrien gagne en 5 coups","Jordan fait autre chose","test3","test4","test 5"];
 var fact = Math.floor(Math.random() * facts.length);
 message.channel.send(facts[fact]);
-    }
   }
 };
