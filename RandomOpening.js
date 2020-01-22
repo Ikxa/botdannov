9abe5550228945d6d124ad73c451835960ed47b8
EidorianP <60190184+EidorianP@users.noreply.github.com>
2020-01-22 18:07:20 +0100

    Add files via upload

--- RandomOpening.js
+++ RandomOpening.js
@@ -2,7 +2,7 @@ module.exports = {
     name: 'RandomOpenings',
     description: "Ouverture d'échecs random",
     execute(message, args) {
-      if (message.includes ("Ouverture Random")) {
+      if (message.content.includes ("Ouverture Random")) {
       var facts = ["Première ouverture","deuxieme ouverture"];
 var fact = Math.floor(Math.random() * facts.length);
 message.channel.send(facts[fact]);
