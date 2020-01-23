f5a520c741585d4d12c95c8c5098af586dd978ab
Jordan Renard <jord.renard@gmail.com>
2020-01-23 19:52:49 +0100

    reset

--- commands/randomopenings.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    name: 'randomopenings',
-    description: "Ouverture d'échecs random",
-    execute(message, args) {
-        var facts = ["Jordan sort son fou et Adrien gagne en 5 coups", "Jordan fait autre chose", "test3", "test4", "test 5"];
-        var fact = Math.floor(Math.random() * facts.length);
-        message.channel.send(facts[fact]);
-    }
-};
