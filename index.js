be2eace0c3b2b91f96389f6d8dbe6cecb9d2d528
Jordan Renard <jord.renard@gmail.com>
2020-03-22 21:29:26 +0100

    Regex

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ bot.on('message', (message) => {
         return;
     }
 
-    let regx = (https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,});
+    let regx = /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/gi;
     let cdu = regx.test(message.content.toLowerCase());
 
     if (cdu === true && message.channel.id != '680739479862247429') {
@@ -35,6 +35,7 @@ bot.on('message', (message) => {
         bot.channels.get('680739479862247429').send(message.content);
         message.delete();
     }
+
 
     if (message.attachments.size > 0 && message.channel.id != '525134186504519680') {
         message.attachments.forEach(attachment => {
