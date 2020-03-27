c1537d6ce801c40a93cca2f6c8bbb2ee569c491a
Jordan Renard <jord.renard@gmail.com>
2020-03-28 00:26:38 +0100

    invite

--- commands/invite.js
+++ commands/invite.js
@@ -1,13 +1,27 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
+const {Client} = require('pg');
+const client = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: true
+});
 
 module.exports = {
     name: 'invite',
     description: "Invitez les autres joueurs",
     execute(message, args) {
-        let currentUserId = message.author.id;
-        let currentUser = bot.fetchUser(currentUserId);
-        console.log(currentUser);
+        let currentUser = message.author;
+        console.log('J\'essaye de récupérer l\'ID de l\'auteur du message');
+
+        client.connect((err, client) => {
+            client.query(
+                'select * from played',
+                (err, result) => {
+                    if (err !== null && err !== '') console.log(err);
+                    console.log(result);
+                }
+            );
+        });
     }
 };
 
