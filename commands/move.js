c0e36a42ed262cee2b520c590bfd2a3985da9f97
Jordan Renard <jord.renard@gmail.com>
2020-03-31 22:57:43 +0200

    wallah

--- commands/move.js
+++ commands/move.js
@@ -2,26 +2,25 @@ module.exports = {
     name: 'mv',
     description: "Rediriger les images et les liens",
     execute(message, args, bot) {
-
-        console.log(args);
-
-        if (args[0].attachments.size > 0 && message.channel.id != '525134186504519680') {
-            args[0].attachments.forEach(attachment => {
-                const url = attachment.url;
-                bot.channels.get('525134186504519680').send('', {file: url});
-                bot.channels.get('525134186504519680').send("<@" + message.author.id + ">, je déplace la photo ici.");
-            });
-            message.delete();
-        }
-
-        let regx = /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/gi;
-        let cdu = regx.test(message.content.toLowerCase());
-
-        if (cdu === true && message.channel.id != '680739479862247429') {
-            if (!args[0].content.startsWith('https://tenor.com')) {
-                bot.channels.get('680739479862247429').send("<@" + message.author.id + ">, je déplace le lien ici.");
-                bot.channels.get('680739479862247429').send(args[0]);
+        if (args[0].attachments.length > 0) {
+            if (args[0].attachments.size > 0 && message.channel.id != '525134186504519680') {
+                args[0].attachments.forEach(attachment => {
+                    const url = attachment.url;
+                    bot.channels.get('525134186504519680').send('', {file: url});
+                    bot.channels.get('525134186504519680').send("<@" + message.author.id + ">, je déplace la photo ici.");
+                });
                 message.delete();
+            }
+        } else {
+            let regx = /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/gi;
+            let cdu = regx.test(message.content.toLowerCase());
+
+            if (cdu === true && message.channel.id != '680739479862247429') {
+                if (!args[0].content.startsWith('https://tenor.com')) {
+                    bot.channels.get('680739479862247429').send("<@" + message.author.id + ">, je déplace le lien ici.");
+                    bot.channels.get('680739479862247429').send(args[0]);
+                    message.delete();
+                }
             }
         }
     }
