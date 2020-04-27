cedfcab3ed28e1a1b45258790ed237c024aaa42b
Jordan Renard <jord.renard@gmail.com>
2020-04-27 20:51:51 +0200

    kick

--- commands/kick.js
+++ commands/kick.js
@@ -3,12 +3,15 @@ module.exports = {
     description: "Kick une personne",
     execute(message, args, bot) {
         let member = message.mentions.members.first();
-        if (member != null) {
+        if (member != null && message.author.id !== '344551142916882442') {
             member.kick().then((member) => {
                 message.channel.send(member.displayName + ' a bien été kick, sah quel plaisir.');
             }).catch(() => {
                 message.channel.send('Permission non accordée.');
             });
+        } else {
+            message.channel.send('Adrien arrête.');
+            return;
         }
     }
 };
