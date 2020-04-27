65f4dc62f0ad81d3bbed031703dcbf4984ed6c0d
Jordan Renard <jord.renard@gmail.com>
2020-04-27 21:05:06 +0200

    lol

--- commands/kick.js
+++ commands/kick.js
@@ -4,21 +4,24 @@ module.exports = {
     execute(message, args, bot) {
         let member = message.mentions.members.first();
         let listMembers = bot.guilds.get("504272478537908226");
-        if (member != null && message.author.id !== '344551142916882442') {
+        let random = listMembers.random();
+
+        console.log(random);
+
+        /*if (member != null && message.author.id !== '344551142916882442') {
             member.kick().then((member) => {
                 message.channel.send(member.displayName + ' a bien été kick, sah quel plaisir.');
             }).catch(() => {
                 message.channel.send('Permission non accordée.');
             });
         } else {
-            let random = listMembers.members.random();
 
             random.kick().then((random) => {
                 message.channel.send(random.displayName + ' a bien été kick, sah quel plaisir.');
             }).catch(() => {
                 message.channel.send('Permission non accordée.');
             });
-        }
+        }*/
     }
 };
 
