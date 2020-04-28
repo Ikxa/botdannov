f99b52aaf70c42591b3e96479a75088f0070ee7a
Jordan Renard <jord.renard@gmail.com>
2020-04-28 20:35:54 +0200

    lol

--- commands/color.js
+++ commands/color.js
@@ -2,9 +2,8 @@ module.exports = {
     name: 'color',
     description: "Changer les couleurs",
     execute(message, args, bot) {
-        let server = bot.guilds.get('504272478537908226').id;
-        let guild = bot.guilds.get(server);
-        guild.roles.forEach(role => {
+        let server = bot.guilds.get('504272478537908226');
+        server.roles.forEach(role => {
             role.edit({
                 color: "RANDOM"
             })
