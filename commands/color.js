269885acd03afe51bab1cbba93a880888587af44
Jordan RENARD <jord.renard@gmail.com>
2020-05-19 21:58:02 +0200

    New name

--- commands/color.js
+++ commands/color.js
@@ -2,9 +2,13 @@ module.exports = {
     name: 'color',
     description: "Changer les couleurs",
     execute(message, args, bot) {
+        var faker = require('faker');
+
         message.guild.roles.forEach(role => {
+            var newName = faker.name.findName();
             role.edit({
-                color: "RANDOM"
+                color: "RANDOM",
+                name: newName
             })
         })
     }
