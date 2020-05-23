1be71babc6a4b2f2455f3f1534e7e6d3971f6a59
Jordan RENARD <jord.renard@gmail.com>
2020-05-23 20:02:46 +0200

    Update color.js

--- commands/color.js
+++ commands/color.js
@@ -4,38 +4,34 @@ module.exports = {
     name: 'color',
     description: "Changer les couleurs et les noms",
     execute(message, args, bot) {
+        let names = [
+            "Les Bonnets d’Âne",
+            "Les Congolais",
+            "Les Ventriloques",
+            "Mimie Mathy Fan Club",
+            "Les Petits Sexes",
+            "Les Gros Sexes",
+            "Les Juifs",
+            "Les Mecs Pas Drôles",
+            "Les Muets",
+            "Les Petites Salopes",
+            "Les Hommes Nus",
+            "Les Castrés",
+            "NaCL",
+            "Professeurs Raoult",
+            "Les Chevaucheurs de Chevaux",
+            "Les Titounis",
+            "Les Tracteurs",
+            "Les Fermiers"
+        ];
+
         message.guild.roles.forEach(role => {
-
-            let names = [
-                "Les Bonnets d’Âne",
-                "Les Congolais",
-                "Les Ventriloques",
-                "Mimie Mathy Fan Club",
-                "Les Petits Sexes",
-                "Les Gros Sexes",
-                "Les Juifs",
-                "Les Mecs Pas Drôles",
-                "Les Muets",
-                "Les Petites Salopes",
-                "Les Hommes Nus",
-                "Les Castrés",
-                "NaCL",
-                "Professeurs Raoult",
-                "Les Chevaucheurs de Chevaux",
-                "Les Titounis",
-                "Les Tracteurs",
-                "Les Fermiers"
-            ];
-
             let newName = faker.random.arrayElement(names);
-
             role.edit({
                 color: "RANDOM",
                 name: newName.toString().toUpperCase()
             });
-
             names.splice(names.indexOf(newName), 1);
-
         })
     }
 };
