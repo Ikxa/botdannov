d33d04e34e8f49a4b27e25481bf5d0ff46f163cd
Jojoséphine <jord.renard@gmail.com>
2020-06-28 00:09:05 +0200

    stream

---
+++ commands/stream.js
@@ -0,0 +1,22 @@
+const Discord = require('discord.js');
+
+module.exports = {
+    name: 'role',
+    description: "Créer un rôle",
+    execute(message, args, bot) {
+
+        const exampleEmbed = new Discord.MessageEmbed()
+            .setColor('#09F')
+            .setTitle('Best streamer EUW')
+            .setURL('https://www.twitch.tv/eidoriaan')
+            .setAuthor('Jojoséphine', 'https://i.imgur.com/wSTFkRM.png', 'https://www.pornhub.com')
+
+        message.channel.send(exampleEmbed);
+
+        /*const role = <guild>.roles.cache.find(role => role.name === '<role name>');
+        const member = <message>.mentions.members.first();
+        member.roles.add(role);*/
+
+    }
+};
+
