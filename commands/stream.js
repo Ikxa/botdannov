b5d5e43d9110a6976ebcd9a19a3139ff38fda2ae
Jojoséphine <jord.renard@gmail.com>
2020-06-28 00:21:12 +0200

    shruhgruhrhu

--- commands/stream.js
+++ commands/stream.js
@@ -13,26 +13,18 @@ module.exports = {
                     name: bot.user.username,
                     icon_url: bot.user.avatarURL
                 },
-                title: "This is an embed",
-                url: "http://google.com",
-                description: "This is a test embed to showcase what they look like and what they can do.",
+                title: "Tu cherches le meilleur streameur EUW ? Clique ici !",
+                url: "http://twitch.tv/eidoriaan",
+                description: "Tu vas vraiment passer un bon moment.",
                 fields: [{
-                    name: "Fields",
-                    value: "They can have different fields with small headlines."
-                },
-                    {
-                        name: "Masked links",
-                        value: "You can put [masked links](http://google.com) inside of rich embeds."
-                    },
-                    {
-                        name: "Markdown",
-                        value: "You can put all the *usual* **__Markdown__** inside of them."
-                    }
+                    name: "Attention",
+                    value: "[Clique pas ici surtout !](http://pornhub.com)"
+                }
                 ],
                 timestamp: new Date(),
                 footer: {
                     icon_url: bot.user.avatarURL,
-                    text: "© Example"
+                    text: "Jojoséphine"
                 }
             }
         });
