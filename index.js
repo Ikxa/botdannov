e1571c8d059ca046de054703b20f35668d2cb26c
Jordan RENARD <jord.renard@gmail.com>
2020-07-18 23:23:24 +0200

    Update index.js

--- index.js
+++ index.js
@@ -50,8 +50,9 @@ bot.on('ready', (message) => {
 
 bot.on('presenceUpdate', (oldMember, newMember) => {
     let adrien = bot.fetchUser('193467165389619211');
-    console.log(adrien.presence.activities);
-    console.log(adrien.presence.game);
+
+    console.log(bot.user.get('EidorianP').id);
+
     if (adrien.presence.game.toString() === 'League of Legends') {
         message.channel.send('<@344551142916882442>, quitte ce jeu immédiatement. Cordialement, la direction.');
     }
