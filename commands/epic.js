95d178b32b956baa03db151cde28563f3a0a69ea
Jordan RENARD <jord.renard@gmail.com>
2021-01-15 21:23:24 +0100

    epic

---
+++ commands/epic.js
@@ -0,0 +1,38 @@
+const fetch = require("node-fetch");
+
+const url = "https://store-site-backend-static.ak.epicgames.com/freeGamesPromotions?locale=fr-FR&country=FR&allowCountries=FR";
+
+module.exports = async () => {
+    let ret = [];
+    let error;
+    try {
+        let res = await fetch(url);
+        let data = await res.json();
+        for (let i = 1; i < data.data.Catalog.searchStore.elements.length; i++) {
+            obj = data.data.Catalog.searchStore.elements[i];
+            const imageUrl = obj.keyImages.reduce((acc, cur) => {
+                if (cur.type === "Thumbnail") {
+                    return cur;
+                } else {
+                    return acc;
+                }
+            }).url;
+
+            if (obj.promotions.upcomingPromotionalOffers.length == 0) {
+                const game = {
+                    title: obj.title,
+                    offerTill: new Date(
+                        obj.promotions.promotionalOffers[0].promotionalOffers[0].endDate
+                    ),
+                    image: imageUrl,
+                    productSlug: obj.productSlug,
+                };
+                ret.push(game);
+            }
+        }
+    } catch (e) {
+        error = e;
+    }
+
+    return {ret, error};
+};
--- index.js
+++ index.js
@@ -3,15 +3,44 @@ const fs = require('fs');
 const bot = new Discord.Client();
 const {Client} = require('pg');
 const axios = require('axios');
+const fetchEpic = require("./commands/epic");
 const client = new Client({
     connectionString: process.env.DATABASE_URL,
     ssl: true
 });
 
+let gameDB = [];
 let prefix = '!';
 let cpt = 0;
 bot.commands = new Discord.Collection();
 const commandFiles = fs.readdirSync('./commands/');
+
+const fetchNewData = async () => {
+    data = await fetchEpic();
+    if (data.error) {
+        // Checking if anything goes wrong 🤢
+        console.log(data.error);
+        return;
+    }
+    gameDB = data.ret;
+};
+
+const sendMessage = (channel) => {
+    if (gameDB.length === 0) {
+        // The empty game array means the data fetching failed
+        channel.send("The bot is offline maybe a maintainace 🤒");
+        return;
+    }
+    gameDB.map((elt) => {
+        const embed = {
+            title: elt.title,
+            thumbnail: { url: elt.image },
+            description: `Offer Ends: ${new Date(elt.offerTill).toLocaleString()}`,
+            url: `https://www.epicgames.com/store/fr-FR/product/${elt.productSlug}`,
+        };
+        channel.send({ embed: embed });
+    });
+};
 
 commandFiles.forEach((file) => {
     const command = require(`./commands/${file}`); // eslint-disable-line
@@ -49,6 +78,12 @@ bot.on('ready', (message) => {
 });
 
 bot.on('message', (message) => {
+    if (message.content.startsWith("~")) {
+        if (message.content === "~epic") {
+            sendMessage(msg.channel);
+        }
+    }
+
     if (message.author.bot) {
         return;
     }
@@ -94,6 +129,11 @@ bot.on('message', (message) => {
         }
     }
 });
+
+(async () => {
+    await fetchNewData();
+})();
+
 
 bot.login(process.env.TOKEN);
 
